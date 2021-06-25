87fd18ba4f672397375b9094eed7cb14bb1f5459
Alison8547 <alisonailson100@hotmail.com>
2021-06-25 14:07:32 -0300

    Developed-get-by-id

--- src/main/java/com/curso/bookstore/controller/BookController.java
+++ src/main/java/com/curso/bookstore/controller/BookController.java
@@ -4,10 +4,7 @@ import com.curso.bookstore.dto.BookDTO;
 import com.curso.bookstore.dto.MessageResponseDTO;
 import com.curso.bookstore.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -25,5 +22,11 @@ public class BookController {
     @PostMapping
     public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
         return bookService.create(bookDTO);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id){
+        return bookService.findById(id);
+
     }
 }
